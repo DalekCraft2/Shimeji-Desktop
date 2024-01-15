eaafd50405bc8ad247256e40a41dbbe5622130a6
DalekCraft <ericdalekcraft@gmail.com>
2024-01-15 16:07:05 -0500

    Remove functions from `User32Extra` and `GDI32Extra` which are already present in JNA

--- src/com/group_finity/mascot/win/jna/GDI32Extra.java
+++ src/com/group_finity/mascot/win/jna/GDI32Extra.java
@@ -15,26 +15,7 @@ public interface GDI32Extra extends StdCallLibrary {
 
     GDI32Extra INSTANCE = Native.load("Gdi32", GDI32Extra.class);
 
-    // WinDef.HDC CreateCompatibleDC(WinDef.HDC hdc);
-
-    // WinNT.HANDLE SelectObject(WinDef.HDC hdc, WinNT.HANDLE h);
-
-    // boolean DeleteDC(WinDef.HDC hdc);
-
-    // int DIB_RGB_COLORS = 0;
-
-    // WinDef.HBITMAP CreateDIBSection(WinDef.HDC hdc, WinGDI.BITMAPINFO pbmi, int usage, Pointer ppvBits, WinNT.HANDLE hSection, int offset);
-
     int GetObjectW(WinNT.HANDLE hgdiobj, int cbBuffer, WinGDI.BITMAP lpvObject);
-
-    // boolean DeleteObject(WinNT.HANDLE hObject);
-
-    // WinDef.HRGN CreateRectRgn(
-    //         int nLeftRect,
-    //         int nTopRect,
-    //         int nRightRect,
-    //         int nBottomRect
-    // );
 
     int GetRgnBox(WinDef.HRGN hrgn, WinDef.RECT lprc);
 }
--- src/com/group_finity/mascot/win/jna/User32Extra.java
+++ src/com/group_finity/mascot/win/jna/User32Extra.java
@@ -13,35 +13,13 @@ public interface User32Extra extends StdCallLibrary {
 
     User32Extra INSTANCE = Native.load("User32", User32Extra.class);
 
-    // int SM_CXSCREEN = 0;
-    // int SM_CYSCREEN = 1;
-
-    // int GetSystemMetrics(int nIndex);
-
     int SPI_GETWORKAREA = 48;
 
     boolean SystemParametersInfoW(int uiAction, int uiParam, WinDef.RECT pvParam, int fWinIni);
 
-    // WinDef.HWND GetForegroundWindow();
-
-    // int GW_HWNDFIRST = 0;
-    // int GW_HWNDNEXT = 2;
-
-    // WinDef.HWND GetWindow(WinDef.HWND hWnd, int uCmd);
-
-    // boolean IsWindow(WinDef.HWND hWnd);
-
-    // boolean IsWindowVisible(WinDef.HWND hWnd);
-
-    // int GWL_STYLE = -16;
-    // int GWL_EXSTYLE = -20;
-
     long GetWindowLongW(WinDef.HWND hWnd, int nIndex);
 
     long SetWindowLongW(WinDef.HWND hWnd, int nIndex, long dwNewLong);
-
-    // int WS_MAXIMIZE = 0x01000000;
-    // int WS_EX_LAYERED = 0x00080000;
 
     boolean IsIconic(WinDef.HWND hWnd);
 
@@ -51,37 +29,9 @@ public interface User32Extra extends StdCallLibrary {
 
     int GetClassNameW(WinDef.HWND hWnd, char[] lpClassName, int nMaxCount);
 
-    // boolean GetWindowRect(WinDef.HWND hWnd, WinDef.RECT lpRect);
-
     int ERROR = 0;
 
     int GetWindowRgn(WinDef.HWND hWnd, WinDef.HRGN hRgn);
 
-    // int MoveWindow(WinDef.HWND hWnd, int X, int Y, int nWidth, int nHeight, boolean bRepaint);
-
-    // boolean BringWindowToTop(WinDef.HWND hWnd);
-
-    // WinDef.HDC GetDC(WinDef.HWND hWnd);
-
-    // int ReleaseDC(WinDef.HWND hWnd, WinDef.HDC hDC);
-
-    // int ULW_ALPHA = 2;
-
-    // boolean UpdateLayeredWindow(WinDef.HWND hWnd, WinDef.HDC hdcDst,
-    //                             WinDef.POINT pptDst, WinUser.SIZE psize,
-    //                             WinDef.HDC hdcSrc, WinDef.POINT pptSrc, int crKey,
-    //                             WinUser.BLENDFUNCTION pblend, int dwFlags);
-
-    // interface WNDENUMPROC extends StdCallCallback {
-    //     /**
-    //      * Returns whether to continue enumeration.
-    //      */
-    //     boolean callback(WinDef.HWND hWnd, Pointer data);
-    // }
-
-    // boolean EnumWindows(WNDENUMPROC lpEnumFunc, Pointer data);
-
     boolean SetProcessDPIAware();
-
-    // boolean GetMonitorInfoA(WinUser.HMONITOR hMonitor, WinUser.MONITORINFO lpmi); // TODO look into for future patches
 }
