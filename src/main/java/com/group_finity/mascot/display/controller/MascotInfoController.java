f4c4be9ab33614480c33a0aeb23ebaf29847c689
Valkryst <github@valkryst.com>
2025-05-15 22:05:44 -0300

    Add MVC classes and enum for displaying `Mascot` information

---
+++ src/main/java/com/group_finity/mascot/display/controller/MascotInfoController.java
@@ -0,0 +1,15 @@
+package com.group_finity.mascot.display.controller;
+
+import com.group_finity.mascot.display.model.MascotInfoModel;
+import com.valkryst.VMVC.controller.Controller;
+
+public class MascotInfoController extends Controller<MascotInfoModel> {
+    /**
+     * Constructs a new {@link MascotInfoController}.
+     *
+     * @param model Model for this controller.
+     */
+    public MascotInfoController(final MascotInfoModel model) {
+        super(model);
+    }
+}
