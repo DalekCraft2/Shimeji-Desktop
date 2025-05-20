4f9e023ab696527c0e2381d4dedf31ee5c7f2e53
Valkryst <github@valkryst.com>
2025-05-20 20:52:45 -0300

    Add debounce timer for the `mouseClicked(MouseEvent)` function of the tray icon's `MouseListener` to prevent the user from creating too many Shimeji in a short period of time

--- src/main/java/com/group_finity/mascot/Main.java
+++ src/main/java/com/group_finity/mascot/Main.java
@@ -20,6 +20,7 @@ import org.xml.sax.SAXException;
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
+import javax.swing.Timer;
 import javax.swing.border.EmptyBorder;
 import javax.swing.event.PopupMenuEvent;
 import javax.swing.event.PopupMenuListener;
@@ -29,7 +30,9 @@ import java.awt.*;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.image.BufferedImage;
-import java.io.*;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.nio.file.Files;
@@ -37,6 +40,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.logging.Level;
 import java.util.logging.LogManager;
 import java.util.logging.Logger;
@@ -395,8 +399,19 @@ public class Main {
 
             // attach menu
             icon.addMouseListener(new MouseListener() {
+                final AtomicBoolean debouncing = new AtomicBoolean(false);
+                final Timer debounceTimer = new Timer(1000, event -> debouncing.set(false));
+
                 @Override
                 public void mouseClicked(MouseEvent e) {
+                    if (debouncing.get()) {
+                        return;
+                    }
+
+                    debouncing.set(true);
+                    debounceTimer.setRepeats(false);
+                    debounceTimer.restart();
+
                     if (SwingUtilities.isLeftMouseButton(e) && !e.isPopupTrigger()) {
                         // Create a mascot when the icon is left-clicked
                         createMascot();
