fa6a8c70c0cb19bf7579de0e70178563fc63d078
DalekCraft <50336832+DalekCraft2@users.noreply.github.com>
2026-01-10 19:16:54 -0500

    Remove `main()` method from `InformationWindow.java`

--- src/main/java/com/group_finity/mascot/InformationWindow.java
+++ src/main/java/com/group_finity/mascot/InformationWindow.java
@@ -248,33 +248,6 @@ public class InformationWindow extends JFrame {
         dispose();
     }// GEN-LAST:event_btnCloseActionPerformed
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see https://docs.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html
-         */
-        try {
-            for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException |
-                 UnsupportedLookAndFeelException ex) {
-            log.log(Level.SEVERE, "Failed to set Look & Feel", ex);
-        }
-        //</editor-fold>
-
-        /* Create and display the form */
-        EventQueue.invokeLater(() -> {
-        });
-    }
-
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private JButton btnClose;
     private JLabel lblSplashImage;
