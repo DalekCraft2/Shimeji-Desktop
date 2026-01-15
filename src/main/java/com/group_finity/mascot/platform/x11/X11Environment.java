fc08378568a08188285760ded3a3521264fc3808
DalekCraft <50336832+DalekCraft2@users.noreply.github.com>
2026-01-14 22:30:06 -0500

    Remove dead code from `X11Environment`

--- src/main/java/com/group_finity/mascot/platform/x11/X11Environment.java
+++ src/main/java/com/group_finity/mascot/platform/x11/X11Environment.java
@@ -5,7 +5,9 @@
 package com.group_finity.mascot.platform.x11;
 
 import com.group_finity.mascot.Main;
-import com.group_finity.mascot.environment.*;
+import com.group_finity.mascot.environment.Area;
+import com.group_finity.mascot.environment.ComplexArea;
+import com.group_finity.mascot.environment.Environment;
 import com.group_finity.mascot.platform.x11.X.Display;
 import com.group_finity.mascot.platform.x11.X.Window;
 import com.group_finity.mascot.platform.x11.X.X11Exception;
@@ -14,7 +16,6 @@ import com.sun.jna.platform.unix.X11;
 import java.awt.*;
 import java.util.*;
 import java.util.List;
-import java.util.stream.Collectors;
 
 /**
  * Uses JNI to obtain environment information that is difficult to obtain with Java.
@@ -28,15 +29,10 @@ class X11Environment extends Environment {
      */
     private final Display display = new Display();
 
-    /**
-     * Hashtable for storing the active windows.
-     */
-    public WindowContainer ieContainer = new WindowContainer();
-
     private final HashMap<Window, Boolean> ieCache = new LinkedHashMap<>();
 
     /**
-     * Randomly chosen window for jump action targeting.
+     * Window for jump action targeting.
      */
     public Area activeIe = new Area();
 
@@ -49,34 +45,8 @@ class X11Environment extends Environment {
      */
     public static final Area workArea = new Area();
 
-    private boolean checkTitles = true;
-    private boolean updateOnNext = false;
-
-    /**
-     * Counter variable used for things which should happen less
-     * frequently than each tick. Initialized at 400 to
-     * force an early {@link #activeIe} selection.
-     */
-    private int q = 400;
-
-    /**
-     * Variables for configuration options.
-     */
-    private int xOffset, yOffset, wMod, hMod = 0;
     private String[] windowTitles = null;
     private String[] windowTitlesBlacklist = null;
-
-    /**
-     * Random number generator for choosing a window for jump actions.
-     */
-    private static final Random RNG = new Random();
-
-    /**
-     * Storage for Window IDs. Only used for comparison when removing
-     * user-terminated windows.
-     */
-    private final Collection<Number> curActiveWin = new ArrayList<>();
-    private final List<Number> curVisibleWin = new ArrayList<>();
 
     /**
      * Storage for values of certain state/type atoms on the current display.
@@ -121,169 +91,19 @@ class X11Environment extends Environment {
         badTypeList.add(display.getAtom("_NET_WM_WINDOW_TYPE_MENU").intValue());
         badTypeList.add(display.getAtom("_NET_WM_WINDOW_TYPE_SPLASH").intValue());
         badTypeList.add(display.getAtom("_NET_WM_WINDOW_TYPE_DIALOG").intValue());
-        // TODO Change this proprietary config format to use the existing one
-        /* try (InputStream fstream = Files.newInputStream(Path.of("window.conf")); DataInputStream in = new DataInputStream(fstream); InputStreamReader inr = new InputStreamReader(in); BufferedReader br = new BufferedReader(inr)) {
-            String strLine;
-            int z = 0;
-            while ((strLine = br.readLine()) != null) {
-                z++;
-                switch (z) {
-                    case 1:
-                        break;
-                    case 2:
-                        xOffset = Integer.parseInt(strLine.trim());
-                        break;
-                    case 3:
-                        yOffset = Integer.parseInt(strLine.trim());
-                        break;
-                    case 4:
-                        wMod = Integer.parseInt(strLine.trim());
-                        break;
-                    case 5:
-                        hMod = Integer.parseInt(strLine.trim());
-                        break;
-                    default:
-                        break;
-                }
-            }
-        } catch (IOException | NumberFormatException ignored) {
-        } */
     }
 
     @Override
     public void tick() {
         super.tick();
-        // TODO Figure out why this has been set to update on every 5th tick.
-        /* if (q % 5 == 0 || updateOnNext) {
-            update();
-        }
-        // New jump action target window every 1000 ticks
-        if (q == 1000) {
-            getRandomIE();
-            q = 0;
-        }
-        q++; */
 
-        // workArea.set(getWorkAreaRect());
+        workArea.set(getWorkAreaRect());
         final Rectangle ieRect = getWindowBounds(findActiveIE());
         activeIe.setVisible(ieRect.intersects(getScreen().toRectangle()));
         activeIe.set(ieRect);
     }
 
-    /**
-     * Window handling. Called each tick.
-     */
-    private void update() {
-        Window[] allWindows;
-        int x, y, w, h, id, curDesktop;
-        Rectangle r;
-        Area a;
-        if (curVisibleWin != null) {
-            curVisibleWin.clear();
-        }
-        curActiveWin.clear();
-        if (display == null) {
-            return;
-        }
-        updateOnNext = false;
-        try {
-            // Retrieve all windows from the X Display
-            // allWindows = display.getWindows();
-            // allWindows = display.getRootWindow().getSubwindows();
-            allWindows = display.getRootWindow().getAllSubwindows();
-
-            try {
-                curDesktop = display.getActiveDesktopNumber();
-            } catch (X11Exception e) {
-                curDesktop = 0;
-            }
-            for (Window window : allWindows) {
-                // Break for-loop if the window title does not match config.
-                if (!isIE(window)) {
-                    // Check checkTitles after isIE() is called because isIE() sets the value of checkTitles.
-                    if (checkTitles) {
-                        continue;
-                    }
-                }
-                // Get window attributes.
-                id = window.getID();
-                Rectangle bounds = window.getBounds();
-                x = bounds.x + xOffset;
-                y = bounds.y + yOffset;
-                w = bounds.width + wMod;
-                h = bounds.height + hMod;
-                if (ieContainer.containsKey(id)) {
-                    a = ieContainer.get(id);
-                    int desktop;
-                    try {
-                        desktop = window.getDesktop();
-                    } catch (X11Exception e) {
-                        desktop = 0;
-                    }
-                    boolean badDesktop = desktop != curDesktop && desktop != -1;
-                    boolean badState = checkState(Arrays.asList(window.getState()));
-                    if (checkTitles) {
-                        if (badDesktop || badState) {
-                            ieContainer.get(id).setVisible(false);
-                        } else {
-                            ieContainer.get(id).setVisible(true);
-                            curVisibleWin.add(id);
-                        }
-                    } else {
-                        boolean badType = checkType(Arrays.asList(window.getType()));
-                        if (badDesktop || badType || badState) {
-                            ieContainer.get(id).setVisible(false);
-                        } else {
-                            ieContainer.get(id).setVisible(true);
-                            curVisibleWin.add(id);
-                        }
-                    }
-                    r = a.toRectangle();
-                    Rectangle newRect = new Rectangle(x, y, w, h);
-                    if (!r.equals(newRect)) {
-                        updateOnNext = true;
-                    }
-                    a.set(newRect);
-                    curActiveWin.add(id);
-                } else {
-                    r = new Rectangle(x, y, w, h);
-                    a = new Area();
-                    a.set(r);
-                    a.setVisible(false);
-                    ieContainer.put(id, a);
-                    curActiveWin.add(id);
-                }
-            }
-        } catch (X11Exception ignored) {
-        }
-        // Remove user-terminated windows from the container every 5th tick
-        for (Map.Entry<Number, Area> entry : ieContainer.entrySet()) {
-            Number i = entry.getKey();
-            if (!curActiveWin.contains(i)) {
-                entry.getValue().setVisible(false);
-                ieContainer.remove(i);
-                break;
-            }
-        }
-    }
-
     private boolean isIE(final Window window) {
-        /* String titleBar = getWindowTitle(window)
-
-        if (windowTitles == null) {
-            windowTitles = new ArrayList<>();
-            windowTitles.addAll(Arrays.asList(Main.getInstance().getProperties().getProperty("InteractiveWindows", "").split("/")));
-            for (int i = 0; i < windowTitles.size(); i++) {
-                if (windowTitles.get(i).trim().isEmpty()) {
-                    windowTitles.remove(i);
-                    i--;
-                }
-            }
-            checkTitles = !windowTitles.isEmpty();
-        }
-
-        return windowTitles.stream().anyMatch(s -> titleBar.toLowerCase().contains(s)); */
-
         final Boolean cachedValue = ieCache.get(window);
         if (cachedValue != null) {
             return cachedValue;
@@ -343,8 +163,6 @@ class X11Environment extends Environment {
     private IeStatus getIeStatus(Window window) {
         int curDesktop;
         int desktop;
-        // int[] state;
-        // int[] type;
         List<Integer> state;
         List<Integer> type;
         try {
@@ -374,8 +192,10 @@ class X11Environment extends Environment {
                 return IeStatus.INVALID;
             }
 
-            // TODO Find some X11 atom which is dedicated to a window being minimized,
-            // because _NET_WM_STATE_HIDDEN is used for both invisible windows and minimized windows
+            /*
+             * TODO Find some X11 atom that is dedicated to a window being minimized,
+             * because _NET_WM_STATE_HIDDEN is used for both invisible windows and minimized windows
+             */
             if (isIE(window) && /* (flags & User32.WS_MINIMIZE) == 0 */ !state.contains(minimizedValue)) {
                 // IE found
                 Rectangle ieRect = getWindowBounds(window);
@@ -438,8 +258,6 @@ class X11Environment extends Environment {
             return new Rectangle();
         }
         return window.getBounds();
-        // Window.Geometry geometry = window.getGeometry();
-        // return new Rectangle(geometry.x + geometry.borderWidth, geometry.y + geometry.borderWidth, geometry.width - 2 * geometry.borderWidth, geometry.height - 2 * geometry.borderWidth);
     }
 
     /**
@@ -464,49 +282,20 @@ class X11Environment extends Environment {
         if (state == null || state.isEmpty()) {
             return true;
         }
-        if (checkTitles) {
-            return state.contains(minimizedValue);
-        } else {
-            return state.stream().anyMatch(badStateList::contains);
-        }
+        return state.contains(minimizedValue);
+        // return state.stream().anyMatch(badStateList::contains);
     }
 
     private boolean checkType(Collection<Integer> type) {
         if (type == null || type.isEmpty()) {
             return true;
         }
-        if (checkTitles) {
-            return false;
-        } else {
-            return type.stream().anyMatch(badTypeList::contains);
-        }
+        return false;
+        // return type.stream().anyMatch(badTypeList::contains);
     }
 
     private Rectangle getWorkAreaRect() {
         return getScreen().toRectangle();
-    }
-
-    /**
-     * Assigns a new randomly selected window to {@link #activeIe}
-     * for jump action targeting.
-     */
-    private void getRandomIE() {
-        if (curVisibleWin == null) {
-            return;
-        }
-        ArrayList<Area> visibleWin = curVisibleWin.stream().filter(Objects::nonNull).map(n -> ieContainer.get(n)).collect(Collectors.toCollection(ArrayList::new));
-        if (visibleWin.isEmpty()) {
-            return;
-        }
-        activeIe = visibleWin.get(RNG.nextInt(visibleWin.size()));
-    }
-
-    public int getDockValue() {
-        return dockValue;
-    }
-
-    public List<Number> getVisible() {
-        return curVisibleWin;
     }
 
     @Override
@@ -573,12 +362,9 @@ class X11Environment extends Environment {
 
     @Override
     public void refreshCache() {
-        ieContainer.clear(); // will be repopulated next isIE call
-        ieCache.clear();
+        ieCache.clear(); // will be repopulated next isIE call
         windowTitles = null;
         windowTitlesBlacklist = null;
-        curActiveWin.clear();
-        curVisibleWin.clear();
     }
 
     @Override
@@ -588,54 +374,5 @@ class X11Environment extends Environment {
     @Override
     public Area getWorkArea() {
         return workArea;
-    }
-
-    /**
-     * Hashtable extension storing {@link Window} IDs and {@link Area} objects
-     * representing the window dimensions. Methods called by
-     * mascots (via {@link MascotEnvironment}) when choosing new actions
-     * to check whether they're on a border of any kind.
-     */
-    public static class WindowContainer extends Hashtable<Number, Area> {
-
-        /**
-         * onBorder, getBorder methods - Called by mascots when
-         * determining the next action. Iterate through all windows,
-         * get the specific border type and call its isOn() to
-         * check whether the mascot anchor is on the border. One
-         * method for plain boolean checks, one for getting the border
-         * when needing to decide movement destinations based on it.
-         */
-        public boolean onLeft(final Point p) {
-            return values().stream().map(area -> new Wall(area, false)).anyMatch(w -> w.isOn(p));
-        }
-
-        public boolean onRight(final Point p) {
-            return values().stream().map(area -> new Wall(area, true)).anyMatch(w -> w.isOn(p));
-        }
-
-        public boolean onTop(final Point p) {
-            return values().stream().map(area -> new FloorCeiling(area, false)).anyMatch(w -> w.isOn(p));
-        }
-
-        public boolean onBottom(final Point p) {
-            return values().stream().map(area -> new FloorCeiling(area, true)).anyMatch(w -> w.isOn(p));
-        }
-
-        public Wall getLeft(final Point p) {
-            return values().stream().map(area -> new Wall(area, false)).filter(w -> w.isOn(p)).findFirst().orElse(null);
-        }
-
-        public Wall getRight(final Point p) {
-            return values().stream().map(area -> new Wall(area, true)).filter(w -> w.isOn(p)).findFirst().orElse(null);
-        }
-
-        public FloorCeiling getTop(final Point p) {
-            return values().stream().map(area -> new FloorCeiling(area, false)).filter(w -> w.isOn(p)).findFirst().orElse(null);
-        }
-
-        public FloorCeiling getBottom(final Point p) {
-            return values().stream().map(area -> new FloorCeiling(area, true)).filter(w -> w.isOn(p)).findFirst().orElse(null);
-        }
     }
 }
