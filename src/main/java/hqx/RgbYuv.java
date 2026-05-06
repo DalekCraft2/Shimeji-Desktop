bf0b5ae404ba31960271833b91f1aa811f8f882c
DalekCraft <ericdalekcraft@gmail.com>
2026-05-06 14:54:55 -0400

    Use static block to initialize hqx RGB to YUV lookup table

--- src/main/java/hqx/RgbYuv.java
+++ src/main/java/hqx/RgbYuv.java
@@ -25,9 +25,23 @@
 
 package hqx;
 
-public final class RgbYuv {
+final class RgbYuv {
     private static final int MASK_RGB = 0x00FFFFFF;
-    private static int[] rgbToYuv = new int[0x1000000];
+    private static final int[] RGB_TO_YUV = new int[0x1000000];
+
+    static {
+        /* Initialize RGB-to-YUV lookup table */
+        int r, g, b, y, u, v;
+        for (int c = 0; c < RGB_TO_YUV.length; c++) {
+            r = (c & 0xFF0000) >>> 16;
+            g = (c & 0x00FF00) >>> 8;
+            b = c & 0x0000FF;
+            y = (int) (+0.299d * r + 0.587d * g + 0.114d * b);
+            u = (int) (-0.169d * r - 0.331d * g + 0.500d * b) + 128;
+            v = (int) (+0.500d * r - 0.419d * g - 0.081d * b) + 128;
+            RGB_TO_YUV[c] = y << 16 | u << 8 | v;
+        }
+    }
 
     /**
      * Returns the 24bit YUV equivalent of the provided 24bit RGB color. <b>Any alpha component is dropped.</b>
@@ -36,32 +50,6 @@ public final class RgbYuv {
      * @return the corresponding 24bit YUV color
      */
     static int getYuv(final int rgb) {
-        return rgbToYuv[rgb & MASK_RGB];
-    }
-
-    /**
-     * Calculates the lookup table. <b>MUST</b> be called (only once) before doing anything else.
-     */
-    public static void hqxInit() {
-        /* Initialize RGB-to-YUV lookup table */
-        if (rgbToYuv == null)
-            rgbToYuv = new int[0x1000000];
-        int r, g, b, y, u, v;
-        for (int c = 0; c < rgbToYuv.length; c++) {
-            r = (c & 0xFF0000) >>> 16;
-            g = (c & 0x00FF00) >>> 8;
-            b = c & 0x0000FF;
-            y = (int) (+0.299d * r + 0.587d * g + 0.114d * b);
-            u = (int) (-0.169d * r - 0.331d * g + 0.500d * b) + 128;
-            v = (int) (+0.500d * r - 0.419d * g - 0.081d * b) + 128;
-            rgbToYuv[c] = y << 16 | u << 8 | v;
-        }
-    }
-
-    /**
-     * Releases the reference to the lookup table. <b>The table has to be calculated again for the next lookup.</b>
-     */
-    public static void hqxDeinit() {
-        rgbToYuv = null;
+        return RGB_TO_YUV[rgb & MASK_RGB];
     }
 }
